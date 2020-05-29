450394cbe9d391189766116d4b2be1d1fec4f9f4
Čestmír Kalina <ckalina@redhat.com>
2020-05-29 04:14:05 +0200

    Add project Makefile.
    Signed-off-by: Čestmír Kalina <ckalina@redhat.com>

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#define _GNU_SOURCE
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <libelf.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <gelf.h>
+
+int main(int argc, const char **argv)
+{
+    return 0;
+}
