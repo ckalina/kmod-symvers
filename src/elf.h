87c149275c303df8b9eeba5bca745447459e4c9f
Čestmír Kalina <ckalina@redhat.com>
2020-05-29 05:09:09 +0200

    Parse and dump symtab/__versions contents.
    Signed-off-by: Čestmír Kalina <ckalina@redhat.com>

--- src/elf.h
+++ src/elf.h
@@ -4,12 +4,21 @@
 # include <libelf.h>
 # include <gelf.h>
 
+# ifndef symtab_for_each
+#  define symtab_for_each(SYMTAB_DATA, STRTAB_DATA, SYMBOL, HASH)       \
+        for (size_t slen = 0;                                           \
+             SYMBOL = STRTAB_DATA->d_buf + ((Elf64_Sym *)symtab.data->d_buf)[slen].st_name, \
+             HASH = &((Elf64_Sym *)symtab.data->d_buf)[slen].st_value, \
+             slen < SYMTAB_DATA->d_size / sizeof(Elf64_Sym);            \
+             slen++)
+# endif
+
 # ifndef __versions_for_each
 #  define __versions_for_each(ELF_DATA, SYMBOL, HASH)                   \
-        for (int __offset =  0;                                         \
+        for (size_t __offset =  0;                                      \
              SYMBOL = ELF_DATA->d_buf + 8 + __offset,                   \
-                     HASH = ELF_DATA->d_buf + __offset,                 \
-                     __offset < ELF_DATA->d_size;                       \
+             HASH = ELF_DATA->d_buf + __offset,                         \
+             __offset < ELF_DATA->d_size;                               \
              __offset += 0x40)
 # endif
 
@@ -25,46 +34,98 @@
         } while ((void)(0))
 # endif
 
-struct elf_section {
-    Elf_Scn *section;
-	Elf_Data *data;
-    uint8_t no_bits : 1;
+struct elf_file {
+        int fd;
+        char *path;
+        Elf *elf;
+        GElf_Ehdr *ehdr;
+        Elf_Scn *scn;
+        size_t shstrndx;
+        unsigned int endian;
 };
 
-static int elf_get_section(Elf *elf, size_t shstrndx, const char *section,
+struct elf_section {
+        Elf_Scn *section;
+        Elf_Data *data;
+        uint8_t no_bits : 1;
+};
+
+static void elf_open(const char *path, struct elf_file *f)
+{
+        int class;
+        Elf_Kind kind;
+
+        if (elf_version(EV_CURRENT) == EV_NONE)
+                fail("outdated ELF library.");
+
+        if ((f->fd = open(path, O_RDWR)) == -1)
+                fail("cannot open %s", path);
+
+        if (asprintf(&f->path, "%s", path) == -1)
+                fail("memory alloc failure");
+
+        f->elf = elf_begin(f->fd, ELF_C_RDWR, NULL);
+
+        if ((kind = elf_kind(f->elf)) != ELF_K_ELF)
+                fail("file is not an ELF object");
+
+        if ((f->ehdr = malloc(sizeof(*f->ehdr))) == NULL)
+                fail("memory alloc failure\n");
+
+        if (gelf_getehdr(f->elf, f->ehdr) == NULL)
+                fail("getehdr: %s\n", elf_errmsg(-1));
+
+        f->endian = f->ehdr->e_ident[EI_DATA];
+
+        if ((class = gelf_getclass(f->elf)) != ELFCLASS64)
+                fail("unsupported ELF class: %d\n", class);
+
+        if (elf_getshdrstrndx(f->elf, &f->shstrndx) != 0)
+                fail("elf_getshdrstrndx: %s\n", elf_errmsg(-1));
+}
+
+static void elf_close(struct elf_file *f)
+{
+        free(f->path);
+        free(f->ehdr);
+        elf_end(f->elf);
+        close(f->fd);
+}
+
+static int elf_get_section(struct elf_file *f, const char *section,
                            struct elf_section *s)
 {
-	Elf_Scn *scn;
-	GElf_Shdr shdr;
-	char *name;
-	Elf_Data *data;
+        Elf_Scn *scn;
+        GElf_Shdr shdr;
+        char *name;
+        Elf_Data *data;
 
-	scn = elf_nextscn(elf, NULL);
-	for (; scn != NULL; scn = elf_nextscn(elf, scn)) {
-		if (gelf_getshdr(scn, &shdr) != &shdr)
-			fail("getshdr: %s\n", elf_errmsg(-1));
+        scn = elf_nextscn(f->elf, NULL);
+        for (; scn != NULL; scn = elf_nextscn(f->elf, scn)) {
+                if (gelf_getshdr(scn, &shdr) != &shdr)
+                        fail("getshdr: %s\n", elf_errmsg(-1));
 
-		name = elf_strptr(elf, shstrndx, shdr.sh_name);
-		if (name == NULL)
-			fail("elf_strptr: %s\n", elf_errmsg(-1));
+                name = elf_strptr(f->elf, f->shstrndx, shdr.sh_name);
+                if (name == NULL)
+                        fail("elf_strptr: %s\n", elf_errmsg(-1));
 
-		if (strcmp(name, section) == 0)
-			break;
-	}
+                if (strcmp(name, section) == 0)
+                        break;
+        }
 
-    if (scn == NULL)
-		return -1;
+        if (scn == NULL)
+                return -1;
 
-    s->section = scn;
-    s->no_bits = shdr.sh_type == SHT_NOBITS;
+        s->section = scn;
+        s->no_bits = shdr.sh_type == SHT_NOBITS;
 
-	if (gelf_getshdr(scn, &shdr) != &shdr)
-		fail("getshdr: %s\n", elf_errmsg(-1));
+        if (gelf_getshdr(scn, &shdr) != &shdr)
+                fail("getshdr: %s\n", elf_errmsg(-1));
 
-	s->data = elf_getdata(scn, NULL);
-	if (s->data == NULL || s->data->d_size == 0)
-		fail("Section %s empty!\n", section);
+        s->data = elf_getdata(scn, NULL);
+        if (s->data == NULL || s->data->d_size == 0)
+                fail("Section %s empty!\n", section);
 
-    return 0;
+        return 0;
 }
 #endif
--- src/main.c
+++ src/main.c
@@ -6,8 +6,50 @@
 #include <stddef.h>
 
 #include "utils.h"
+#include "elf.h"
 
 int main(int argc, const char **argv)
 {
+    char *symbol;
+    uint32_t *hash;
+    struct elf_file file;
+    struct elf_section strtab, symtab, __versions;
+
+	if (argc < 2 || argc > 2 && argc % 2 != 0)
+		fail("Usage: %s elf_file [SYMBOL NEW_CHECKSUM]...", argv[0]);
+
+    elf_open(argv[1], &file);
+
+    elf_get_section(&file, "__versions", &__versions);
+    elf_get_section(&file, ".strtab", &strtab);
+    elf_get_section(&file, ".symtab", &symtab);
+
+    if (symtab.data->d_size % sizeof(Elf64_Sym) != 0)
+        fail("unexpected size of symtab section");
+
+    int i = argc;
+    while (i-->1) {
+        symtab_for_each(symtab.data, strtab.data, symbol, hash) {
+            if (strncmp(symbol, "__crc", 5) != 0)
+                continue;
+            if (i == 1) {
+                printf("%08p\t%-40s\tsymtab\n", *hash, symbol);
+            }
+            if (strcmp(symbol, argv[i]) == 0)
+                *hash = strtol(argv[i+1], NULL, 16);
+        }
+
+        __versions_for_each(__versions.data, symbol, hash) {
+            if (i == 1) {
+                printf("%08p\t%-40s\t__versions\n", *hash, symbol);
+                continue;
+            }
+            if (strcmp(symbol, argv[i]) == 0)
+                *hash = strtol(argv[i+1], NULL, 16);
+        }
+    }
+
+    elf_close(&file);
+
     return 0;
 }
